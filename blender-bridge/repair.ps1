7acf09eb5e3d53e0a53f11e2abfa2eafc7d3a485
sapphiremaid <103682843+sapphiremaid@users.noreply.github.com>
2026-08-28 05:05:14 -0700

    Migrate Pages URLs after GitHub username change

--- blender-bridge/repair.ps1
+++ blender-bridge/repair.ps1
@@ -1,4 +1,4 @@
 # Repair Katherine's Blender bridge using the silent pythonw supervisor.
 $ErrorActionPreference = 'Stop'
-$installer = Invoke-RestMethod 'https://raw.githubusercontent.com/amuletmaiden/amuletmaiden.github.io/main/blender-bridge/install.ps1'
+$installer = Invoke-RestMethod 'https://raw.githubusercontent.com/sapphiremaid/sapphiremaid.github.io/main/blender-bridge/install.ps1'
 Invoke-Expression $installer
